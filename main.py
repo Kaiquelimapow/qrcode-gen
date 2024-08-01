86e2c4eedea2d2f4dce9c97a22cdf39b39006569
Kaiquelimapow <kaiquelima1011@gmail.com>
2024-08-01 12:20:50 -0300

    aula qr code

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def gerar_qrcode():
             qr = qrcode.QRCode(version=1, box_size=10, border=5)
             qr.add_data(url)
             qr.make(fit=True)
-            img = qr.make_image(fill_color='black, back_color=white')
+            img = qr.make_image(fill_color='black', back_color='white')
             img.save('qrcode.png')
 
 if __name__ == '__main__':
